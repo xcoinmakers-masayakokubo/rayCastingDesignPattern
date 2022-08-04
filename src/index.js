7ce6742b42a3ddfa4d5bd314810fb87c4cf145b3
m.kokubo <m.kokubo@exchangers.co.jp>
2022-08-04 18:44:40 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import p5 from 'p5';
+import { sketch } from './sketch.js';
+
+document.body.style.margin = "0";
+
+const app = new p5(sketch, document.body);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/js'),
+    publicPath: 'public',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devServer: {
+    hot: true,
+    open: true
+  },
+};
